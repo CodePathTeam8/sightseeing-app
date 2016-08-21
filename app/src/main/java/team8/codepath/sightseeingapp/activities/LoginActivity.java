dd893def645870ef1f8092c49cf0395ecb93b00e
Isabel Palomar <isabel.palomarb@gmail.com>
2016-08-21 10:32:39 -0500

    User can sign in using Facebook OAuth login

--- app/src/main/java/team8/codepath/sightseeingapp/activities/LoginActivity.java
+++ app/src/main/java/team8/codepath/sightseeingapp/activities/LoginActivity.java
@@ -1,36 +1,68 @@
 package team8.codepath.sightseeingapp.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import team8.codepath.sightseeingapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btnLogin;
+    private static final String TAG = "LOGIN DEBUG";
+    @BindView(R.id.btnFacebookLogin)
+    LoginButton btnFacebookLogin;
+
+    CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        btnFacebookLogin.setReadPermissions("email", "public_profile");
+        btnFacebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                onLoginSuccess();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void onLoginSuccess() {
         Intent i = new Intent(this, TripListActivity.class);
         startActivity(i);
-    }
-
-
-    public void login(View view){
-        //
-        // Login Logic Here
-        //
-
-        // On Success
-        onLoginSuccess();
     }
 
 }

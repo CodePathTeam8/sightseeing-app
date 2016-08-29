f1e91411d09e455044dfd283598f20ec14a0be2a
Isabel Palomar <isabel.palomarb@gmail.com>
2016-08-29 15:46:50 -0500

    TripList migrated from ListView to RecyclerView

--- app/src/main/java/team8/codepath/sightseeingapp/adapters/TripsRecyclerAdapter.java
+++ app/src/main/java/team8/codepath/sightseeingapp/adapters/TripsRecyclerAdapter.java
@@ -1,7 +1,10 @@
 package team8.codepath.sightseeingapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,29 +15,33 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.squareup.picasso.Picasso;
 
+import org.parceler.Parcels;
+
 import team8.codepath.sightseeingapp.R;
+import team8.codepath.sightseeingapp.activities.TripDetailsActivity;
 import team8.codepath.sightseeingapp.models.TripModel;
 
 /**
  * Created by floko_000 on 8/18/2016.
  */
-public class TripsArrayAdapter extends FirebaseRecyclerAdapter<TripModel,
-        TripsArrayAdapter.ViewHolder> {
+public class TripsRecyclerAdapter extends FirebaseRecyclerAdapter<TripModel,
+        TripsRecyclerAdapter.ViewHolder> {
 
-    private Context context;
+    private Context mContext;
 
-    public TripsArrayAdapter(int modelLayout, DatabaseReference ref) {
-        super(TripModel.class, modelLayout, TripsArrayAdapter.ViewHolder.class, ref);
+    public TripsRecyclerAdapter(int modelLayout, DatabaseReference ref) {
+        super(TripModel.class, modelLayout, TripsRecyclerAdapter.ViewHolder.class, ref);
     }
 
     // Easy access to the context object in the recyclerview
     private Context getContext() {
-        return context;
+        return mContext;
     }
 
     @Override
-    public TripsArrayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TripsRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
+        mContext = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
         // Inflate the custom layout
@@ -73,15 +80,14 @@ public class TripsArrayAdapter extends FirebaseRecyclerAdapter<TripModel,
         tvTripLength.setText(trip.getTotalLength());
 
 
-
         // 5. Return the view to be inserted into the list
         return convertView;
 
     }
 
     @Override
-    public void onBindViewHolder(TripsArrayAdapter.ViewHolder viewHolder, int position) {
-        TripModel trip = getItem(position);
+    public void onBindViewHolder(TripsRecyclerAdapter.ViewHolder viewHolder, final int position) {
+        final TripModel trip = getItem(position);
 
         ImageView bannerView = viewHolder.banner;
 
@@ -91,6 +97,15 @@ public class TripsArrayAdapter extends FirebaseRecyclerAdapter<TripModel,
         viewHolder.name.setText(trip.getName());
         viewHolder.distance.setText(trip.getDistance());
         viewHolder.length.setText(trip.getTotalLength());
+        viewHolder.cvTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), TripDetailsActivity.class);
+
+                intent.putExtra("trip", Parcels.wrap(trip));
+                getContext().startActivity(intent);
+            }
+        });
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
@@ -98,6 +113,7 @@ public class TripsArrayAdapter extends FirebaseRecyclerAdapter<TripModel,
         public TextView name;
         public TextView distance;
         public TextView length;
+        public CardView cvTrip;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -105,6 +121,7 @@ public class TripsArrayAdapter extends FirebaseRecyclerAdapter<TripModel,
             name = (TextView) itemView.findViewById(R.id.tvTripName);
             distance = (TextView) itemView.findViewById(R.id.tvTripDistance);
             length = (TextView) itemView.findViewById(R.id.tvTripLength);
+            cvTrip = (CardView) itemView.findViewById(R.id.cvTrip);
         }
 
     }
